64fb2473ba8d4ac1fe3c8919fcefd13bd40c47fe
fpstorm3 <fpstorm3@gmail.com>
2021-10-10 17:10:36 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+from logging import debug
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello, world !"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
